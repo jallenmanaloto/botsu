69599cc1490bc70c55b5e9dc4de269810ddf0c9f
jallenmanaloto <jallen.manaloto@gmail.com>
2023-04-28 03:24:37 -0500

    Uninstall unnecessary packages

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,10 +5,24 @@ import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
 import { BotModule } from './bot/bot.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { dataSourceOptions } from 'ormconfig';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOptions), UserModule, BotModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: ['dist/**/*.entity.js'],
+      migrations: ['dist/src/migration/*.js']
+    }),
+    UserModule,
+    BotModule
+  ],
   controllers: [AppController, UserController],
   providers: [AppService],
 })
