4d5193499f70f01f998628fbc67cf13e1bdbb20c
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2023-04-30 15:28:05 +0800

    Add create new user and user session

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,14 +1,40 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, Req, UseGuards, Res, HttpStatus } from '@nestjs/common';
 import { User } from 'src/entities/user.entity';
 import { UserService } from './user.service';
+import { AuthGuard } from '@nestjs/passport';
+import { AuthenticatedRequest } from '../auth/auth.interface'
+import { CreateUserDto } from './dto/create-user-dto';
+import { Response } from 'express';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) { }
 
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto, @Res() res: Response): Promise<Response> {
+
+    const newUser = await this.userService.create(createUserDto);
+    if (newUser) return res.status(HttpStatus.CREATED)
+      .json({
+        message: 'Successfully created a new user',
+        user: newUser
+      });
+
+    return res.status(HttpStatus.BAD_REQUEST)
+      .json({
+        message: 'Unable to create a new user',
+      });
+  }
+
   @Get()
   async findOne(@Query('email') email: string): Promise<User> {
     return await this.userService.findOne(email);
+  }
+
+  @Get('session')
+  @UseGuards(AuthGuard('jwt'))
+  getUserSession(@Req() req: AuthenticatedRequest) {
+    return req.user;
   }
 
 }
--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -2,10 +2,19 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../entities/user.entity'
 import { UserRepository } from './user.repository';
+import { CreateUserDto } from './dto/create-user-dto';
 
 @Injectable()
 export class UserService {
   constructor(@InjectRepository(User) private userRepository: UserRepository) { }
+
+  create(createUserDto: CreateUserDto): Promise<User> {
+    const newUser = this.userRepository.create(createUserDto);
+    const savedUser = this.userRepository.save(newUser);
+
+    return savedUser;
+  }
+
 
   async findOne(email: string): Promise<User> {
     return this.userRepository.findOne({ where: { email: email } })
