be5509e4918fe0cb6a6c3efb758b29c61c05a07d
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2023-05-01 11:23:59 +0800

    Fixed github linking

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -5,16 +5,23 @@ import { useModeStore } from '../utils/store'
 
 export default function Header() {
 	const { mode, setMode } = useModeStore()
-	const handleChangeMode = () => {
+	const handleChangeMode = (): void => {
 		const newMode = mode === 'dark' ? 'light' : 'dark'
 		setMode(newMode)
+	}
+
+	const openGithub = (): void => {
+		window.open('https://github.com/jallenmanaloto/botsu', '_blank')
 	}
 
 	return (
 		<div className="container lg text-lg flex justify-between">
 			<h1 className="p-5 text-5xl font-comfortaa font-semibold">Botsu</h1>
 			<div className="w-28 p-5 flex justify-around items-center">
-				<GitHubIcon className="cursor-pointer w-7 h-7" />
+				<GitHubIcon
+					onClick={() => openGithub()}
+					className="cursor-pointer w-7 h-7"
+				/>
 				{mode === 'dark' ? (
 					<DarkModeIcon
 						onClick={() => handleChangeMode()}
