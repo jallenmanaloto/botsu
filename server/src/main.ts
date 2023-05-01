e2770b7281c0a7a670fda738e4d5f1570c93fd8b
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2023-05-01 12:43:15 +0800

    Updated cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,7 +6,12 @@ import 'reflect-metadata'
 async function bootstrap() {
   dotenv.config()
 
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET, PUT, POST, DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
   await app.listen(8000);
 }
 bootstrap();
