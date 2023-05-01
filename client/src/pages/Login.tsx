f20eaefba27b16c040458d764d3b40bc99988e0b
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2023-05-01 13:01:12 +0800

    Add login auth

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,29 +1,60 @@
-import { useModeStore } from '../utils/store'
+import { useModeStore, useLoginStore } from '../utils/store'
+import { useState } from 'react'
+import axios from 'axios'
 
 export default function Login() {
 	const { mode } = useModeStore()
+	const { setToken } = useLoginStore()
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+
+	const handleLogin = async (e: React.MouseEvent) => {
+		e.preventDefault()
+		const data = {
+			email: email,
+			password: password,
+		}
+
+		const config = {
+			method: 'POST',
+			url: import.meta.env.VITE_LOGIN_URL,
+			data: data,
+		}
+
+		try {
+			const loginResponse = await axios(config)
+			setToken(loginResponse?.data)
+		} catch (error) {
+			console.log(error)
+		}
+	}
 
 	return (
 		<div className="container lg h-full text-lg flex justify-center items-center">
 			<div className="flex flex-col pt-24 items-center w-10/12 h-3/4 md:h-5/6 md:w-5/6 lg:h-2/3 lg:w-4/6 xl:h-2/3 xl:w-7/12 2xl:h-2/3 2xl:w-5/12">
 				<h2 className="text-4xl font-semibold font-comme py-4">Sign in</h2>
 				<input
+					onChange={(e) => setEmail(e.target.value)}
+					value={email}
 					className={`w-full h-20 my-4 pl-5 rounded-lg outline-none xs:max-mdtext-sm lg:text-xl font-comme ${
 						mode === 'dark'
-							? 'bg-gray-700 text-white placeholder-white'
+							? 'bg-gray-700 text-white placeholder-slate-500'
 							: 'border-2 border-b-slate-200'
 					}`}
 					placeholder="Email"
 				/>
 				<input
+					onChange={(e) => setPassword(e.target.value)}
+					value={password}
 					className={`w-full h-20 my-4 pl-5 rounded-lg outline-none xs:max-mdtext-sm lg:text-xl font-comme ${
 						mode === 'dark'
-							? 'bg-gray-700 text-white placeholder-white'
+							? 'bg-gray-700 text-white placeholder-slate-500'
 							: 'border-2 border-b-slate-200'
 					}`}
 					placeholder="Password"
 				/>
 				<button
+					onClick={(e) => handleLogin(e)}
 					className={`w-full h-20 my-4 bg-gray-800 rounded-lg text-xl font-semibold font-comme ${
 						mode === 'dark'
 							? 'text-slate-100 hover:bg-blue-950 bg-blue-900'
--- client/src/utils/store.ts
+++ client/src/utils/store.ts
@@ -10,3 +10,18 @@ export const useModeStore = create<Mode>((set) => ({
   mode: 'dark',
   setMode: (mode: ModeType) => set({ mode: mode })
 }));
+
+type LoginResponse = {
+  message: string
+  access_token: string
+}
+
+type Login = {
+  token: string;
+  setToken: (response: LoginResponse) => void
+}
+
+export const useLoginStore = create<Login>((set) => ({
+  token: '',
+  setToken: (response: LoginResponse) => set({ token: response.access_token })
+}))
